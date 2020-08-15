a1b76ed27f4831f4c139d703f2a10e9c07aa9215
jirenmaa <jirenmaa@gmail.com>
2020-08-15 18:31:22 +0700

    update total number of proposals

--- components/dashboard/umkm/Statistics.vue
+++ components/dashboard/umkm/Statistics.vue
@@ -3,7 +3,7 @@
     <div class="w-1/2">
       <div class="px-12 py-10 bg-white shadow-md rounded-lg">
         <h1 class="font-medium text-4xl">Total Proposals</h1>
-        <h1 class="font-bold text-6xl">4</h1>
+        <h1 class="font-bold text-6xl">5</h1>
       </div>
     </div>
   </div>
