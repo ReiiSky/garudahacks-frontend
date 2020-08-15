d272e9c104a72e75741bdaa0a5f2ed7955cbbc06
jirenmaa <jirenmaa@gmail.com>
2020-08-15 15:44:12 +0700

    refactor: use vue-carousel component

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,22 @@
+/* eslint-disable prettier/prettier */
 <template>
   <div>
-    <Carousel />
+    <div class="mt-12">
+      <carousel
+        :per-page="1"
+        :mouse-drag="false"
+        :pagination-color="'#c5c5c5'"
+        :pagination-position="'bottom-overlay'"
+        :pagination-active-color="'#00aff5'"
+      >
+        <slide>
+          <img src="~/assets/images/lan_umkm-1.jpg" class="carousel-image" />
+        </slide>
+        <slide>
+          <img src="~/assets/images/lan_umkm-1.jpg" class="carousel-image" />
+        </slide>
+      </carousel>
+    </div>
     <Container class="mt-24 mb-24">
       <div class="flex flex-wrap">
         <div class="w-full">
@@ -13,12 +29,17 @@
 
 <script>
 import Hero from '~/components/home/Hero'
-import Carousel from '~/components/home/Carousel'
 
 export default {
   components: {
     Hero,
-    Carousel,
   },
 }
 </script>
+
+<style scoped>
+.carousel-image {
+  width: 100%;
+  height: 35em !important;
+}
+</style>
