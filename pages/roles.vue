6fcd27cd66c130903ebdeff1484f1d66293855da
jirenmaa <jirenmaa@gmail.com>
2020-08-15 20:13:49 +0700

    refactor: delete investor role card

--- pages/roles.vue
+++ pages/roles.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Container class="mt-24 mb-12">
-      <h1 class="lg:text-4xl">Pilih role anda</h1>
+      <h1 class="lg:text-4xl">Choose your role</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum est
         distinctio veritatis temporibus nemo at doloribus quidem inventore
@@ -9,10 +9,9 @@
         praesentium possimus tempora odio.
       </p>
       <div
-        class="container mx-auto px-4 sm:px-10 lg:px-15 mt-24 role-card flex flex-wrap justify-between"
+        class="container mx-auto px-4 sm:px-10 lg:px-15 mt-24 flex flex-wrap justify-between"
       >
         <div id="umkm" class="card bg-white rounded-lg shadow-lg">
-          <!-- <h3 class="lg:text-2xl text-center">UMKM</h3> -->
           <img
             src="~/assets/images/umkm.jpg"
             alt="umkm image"
@@ -25,38 +24,13 @@
               Exercitationem nostrum minus hic sunt tempore aspernatur tempora
               quis atque.
             </p>
-            <p class="mt-12 text-center">Daftar sebagai</p>
+            <p class="mt-12 text-center">Register as</p>
             <Button class="w-full">
               <a href="/register?as=umkm" class="w-full px-12 py-2">UMKM</a>
             </Button>
           </div>
         </div>
-        <div id="investor" class="card bg-white rounded-lg shadow-lg">
-          <!-- <h3 class="lg:text-2xl text-center">Pemodal</h3> -->
-          <img
-            src="~/assets/images/investor.jpg"
-            alt="investor image"
-            srcset
-            class="h-48 rounded-t-lg w-full"
-          />
-          <div class="card-content p-3">
-            <p class="px-2">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Exercitationem nostrum minus hic sunt tempore aspernatur tempora
-              quis atque.
-            </p>
-            <p class="mt-12 text-center">Daftar sebagai</p>
-            <div class="items-center">
-              <Button class="w-full">
-                <a href="/register?as=investor" class="w-full px-12 py-2">
-                  Pemodal
-                </a>
-              </Button>
-            </div>
-          </div>
-        </div>
         <div id="company" class="card bg-white rounded-lg shadow-lg">
-          <!-- <h3 class="lg:text-2xl text-center">Perusahaan</h3> -->
           <img
             src="~/assets/images/company.jpg"
             alt="company image"
@@ -69,10 +43,10 @@
               Exercitationem nostrum minus hic sunt tempore aspernatur tempora
               quis atque.?
             </p>
-            <p class="mt-12 text-center">Daftar sebagai</p>
+            <p class="mt-12 text-center">Register as</p>
             <Button class="w-full">
               <a href="/register?as=company" class="w-full px-12 py-2">
-                Perusahaan
+                Company
               </a>
             </Button>
           </div>
@@ -90,7 +64,7 @@ export default {
 
 <style scoped>
 .card {
-  width: 16rem;
+  width: 17rem;
   height: auto;
 }
 </style>
